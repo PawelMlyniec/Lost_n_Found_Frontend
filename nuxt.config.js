0842cc2a2bbb8097d64bbb0bf0ba2046b97a8a6e
Grzegorz Kuduk <grzegorz07@gmail.com>
2021-04-12 13:44:00 +0200

    add baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://localhost:8080/api',
     // credentials: true,
   },
 
