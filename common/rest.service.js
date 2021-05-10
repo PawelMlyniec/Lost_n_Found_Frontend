ec2761dbc05a5187cd2ba08629cab42d329d9c07
Raciniewska <raciniewska.barbara@gmail.com>
2021-05-10 07:26:23 +0200

    hot fix axios

--- common/rest.service.js
+++ common/rest.service.js
@@ -2,19 +2,19 @@
 
 export default {
   getLostItem(axios, id) {
-    return axios.get(`/lostReports/${id}`)
+    return axios.get(`http://34.98.81.177/lostReports/${id}`)
   },
 
   getLostItems(axios) {
-    return axios.get('/lostReports')
+    return axios.get('http://34.98.81.177/lostReports')
   },
 
   resolveLostItem(axios, id) {
-    return axios.post(`/lostReports/${id}/resolve`)
+    return axios.post(`http://34.98.81.177/lostReports/${id}/resolve`)
   },
 
   deleteLostItem(axios, id) {
-    return axios.delete(`/lostReports/${id}`)
+    return axios.delete(`http://34.98.81.177/lostReports/${id}`)
   },
   getLostItems(axios, page, size, body){
     return axios.post('http://34.98.81.177/lostReports/searches?page='+page+'&size='+size, body)
